7b5d54c79b217af53141810fae04a9641fb86a5c
elijah <elijah_burke@hotmail.co.uk>
2021-01-26 16:23:49 +0000

    Updates

---
+++ precache-manifest.0c5dd39191c296ec5c248188ecf9fa3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754830a7c52dc51a3f0000921fa06a5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d2ac789e33722238ce8",
+    "url": "./static/css/2.dd48bb52.chunk.css"
+  },
+  {
+    "revision": "21e277d6d6f9d9109635",
+    "url": "./static/css/main.e3c9028c.chunk.css"
+  },
+  {
+    "revision": "8d2ac789e33722238ce8",
+    "url": "./static/js/2.14e8a9e0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.14e8a9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e277d6d6f9d9109635",
+    "url": "./static/js/main.3927ca30.chunk.js"
+  },
+  {
+    "revision": "6dee5f01443dbc57c222",
+    "url": "./static/js/runtime-main.ea5269cc.js"
+  }
+]);
